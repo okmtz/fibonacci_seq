3ea22f5c83699ed09432940b934952f9b2b3385c
okmtz <yuto430scuderia@gmail.com>
2021-05-05 22:31:29 +0900

    :+1: add test case

--- fibonacci_seq/fibonacci_seq.py
+++ fibonacci_seq/fibonacci_seq.py
@@ -1,3 +1,4 @@
-def sum_fib_seq(n):
+def fib_num(n):
     if (n == 0) or (n == 1):
         return n
+    return fib_num(n-1) + fib_num(n-2)
--- tests/test_fibonacci_seq.py
+++ tests/test_fibonacci_seq.py
@@ -1,15 +1,15 @@
 from fibonacci_seq import __version__
 import unittest
 from unittest import TestCase
-from fibonacci_seq.fibonacci_seq import sum_fib_seq
+from fibonacci_seq.fibonacci_seq import fib_num
 
 
 class FibonacciTest(TestCase):
 
     def test_fibonaci(self):
-        test_cases = [[0,0], [1,1]]
+        test_cases = [[0, 0], [1, 1], [1, 2], [2, 3], [6765, 20]]
         for i in test_cases:
-            self.assertEqual(i[0], sum_fib_seq(i[1]))
+            self.assertEqual(i[0], fib_num(i[1]))
 
 
 if __name__ == "__main__":
